b721be56eab00a3adec9a7abf6f259de698698c5
weterynarzfred <weterynarzfred@gmail.com>
2019-03-17 12:05:13 +0100

    refactor js

--- include/plugin/reader/js/reader.js
+++ include/plugin/reader/js/reader.js
@@ -1,157 +1,218 @@
+// reader_manga.php
 {
-$(document).on('mousedown', '.reader-page', function(e) {
-  let target;
-  if(e.which === 1) {
-		let el = $(this).next();
-		if(el.length > 0) target = el.offset().top;
-	}
-  else if(e.which === 3) {
-		let el = $(this).prev();
-    if(el.length > 0) target = el.offset().top;
-	}
-  if(target !== void 0) {
-    $('html, body').stop().animate({scrollTop:target}, 200);
-  }
-});
 
-$(document).on('keydown', function(e) {
-	if(currentView === 'reader_manga') {
-		if(e.which === 32 || e.which === 39 || e.which === 40) {
-			e.preventDefault();
-			const target = window.scrollY + $(window).height();
-			$('html, body').stop().animate({scrollTop:target}, 200);
+	let pages = [];
+	let lastReadPage = 0;
+	let resetLastReadPage = false;
+	let isEnabled = false;
+	let isAutoScrolling = false;
+
+	window.addEventListener('afterNavigation', function() {
+	  if(!isEnabled && currentView === 'reader_manga') {
+			start();
 		}
-		else if(e.which === 8 || e.which === 37 || e.which === 38) {
-			e.preventDefault();
-			const target = window.scrollY - $(window).height();
-			$('html, body').stop().animate({scrollTop:target}, 200);
+	});
+	window.addEventListener('beforeNavigation', function() {
+		if(isEnabled && currentView !== 'reader_manga') {
+			stop();
 		}
-	}
-});
+	});
 
-$(document).on('contextmenu', function(e) {
-  e.preventDefault();
-});
-
-let pages;
-let pagesArray;
-let lastReadPage = 0;
-let resetLastReadPage = false;
-
-window.addEventListener('beforeNavigation', function(e) {
-  if(pages !== void 0 && e.detail.name !== 'reader_manga') {
-    pages = void 0;
-		$(window).off('.reader');
-  }
-});
-
-window.addEventListener('afterNavigation', function(e) {
-  if(e.detail.name === 'reader_manga') {
-    pages = $('.reader-page');
-		pagesArray = pages.map(function() {
+	function start() {
+    pages = $('.reader-page').map(function() {
 			return {
 				el	:	$(this),
 				offset	:	0,
 			}
 		});
     fitMangaPage();
-		if(resetLastReadPage) {
-			resetLastReadPage = false;
-		}
-		else {
+		if(!resetLastReadPage) {
 			lastReadPage = $('.reader-manga-pages-container').data('last-read-page');
-			if(lastReadPage === '') lastReadPage = 0;
-			if(lastReadPage > 0) {
-				$('html, body').animate({scrollTop: pagesArray[lastReadPage].offset}, 1000);
+			if(lastReadPage === '') {
+				lastReadPage = 0;
+			}
+			else if(lastReadPage > 0) {
+				isAutoScrolling = true;
+				saveLastReadPage(lastReadPage);
+				$('html, body').animate(
+					{scrollTop: pages[lastReadPage].offset},
+					1000,
+					function() {isAutoScrolling = false;},
+				);
 			}
 		}
+		resetLastReadPage = false;
 
 		$(window)
-			.on('scroll.reader', throttle(1000, function() {
-				let pageId = 0;
-				for(let i = lastReadPage + 1; i < pagesArray.length; i++) {
-					if(pagesArray[i].offset > window.scrollY + 10) {
-						pageId = i - 1;
-						break;
-					}
-				}
-				if(pageId > lastReadPage) {
-					lastReadPage = pageId;
-					doQuery({
-				    data  : {
-				      action	:	'update_entry',
-				      values	:	{
-								id	:	$('.reader-manga-pages-container').data('id'),
-								last_read_page	:	pageId,
-							},
-				    },
-						filter	:	function(data) {
-							data.fragments = data.fragments.filter(function(e) {
-								return e.type !== 'message';
-							});
-							return data;
-						},
-				  });
+			.on('scroll.reader_manga', throttle(1000, function() {
+				if(!isAutoScrolling) {
+					saveLastReadPage();
 				}
 			}))
-			.on('resize.reader', throttle(100, function() {
-			    fitMangaPage();
-			}));
-  }
-});
+			.on('resize.reader_manga', throttle(100, fitMangaPage))
+			.on('contextmenu.reader_manga', function(e) {
+			  e.preventDefault();
+			});
 
-function fitMangaPage() {
-  pages.css({height: $(window).height()});
-	pagesArray.map(function() {
-		this.offset = this.el.offset().top;
-	});
-}
-
-// last read page
-$(document).on('click', '.reader-manga-file', function() {
-	const t = $(this);
-	let text = t.text();
-	text = text.replace(/(^\s*|\s*$)/g,'');
-	const isLastRead = t.find('.reader-manga-file-last-read').length > 0;
-	if(!isLastRead) {
-		let args = {
-	    data  : {
-	      action	:	'update_entry',
-	      values	:	{
-					id	:	t.parents('.reader-manga-file-list').data('id'),
-					last_read_file	:	text,
-					last_read_page	:	0.
-				},
-	    },
-			filter	:	function(data) {
-				data.reader_skip_refresh = true;
-				return data;
+		$(document).on('mousedown.reader_manga', '.reader-page', function(e) {
+		  if(e.which === 1) {
+				ScrollToAdjacent(1);
 			}
-	  };
-		resetLastReadPage = true;
-		doQuery(args);
+		  else if(e.which === 3) {
+				ScrollToAdjacent(-1);
+			}
+		})
+		.on('keydown.reader_manga', function(e) {
+			if(currentView === 'reader_manga') {
+				if(e.which === 32 || e.which === 39 || e.which === 40) {
+					e.preventDefault();
+					ScrollToAdjacent(1);
+				}
+				else if(e.which === 8 || e.which === 37 || e.which === 38) {
+					e.preventDefault();
+					ScrollToAdjacent(-1);
+				}
+			}
+		})
+		isEnabled = true;
 	}
-});
 
-// refresah view on read progress update
-window.addEventListener('ajaxRequestDone', function(e) {
-	if(!e.detail.reader_skip_refresh) {
-		if(currentView === 'reader') {
-			if(e.detail.type === 'update_entry') {
-				if(e.detail.success) {
-					doQuery({
-						data	: {
-							action	:	'display_view',
-							values	:	{
-								name	:	'reader',
-								value	:	$('.reader-manga-file-list').data('id'),
-							},
-						}
-					});
+	function stop(event) {
+		saveLastReadPage();
+		$(window).off('.reader_manga');
+		$(document).off('.reader_manga');
+    pages = [];
+		isEnabled = false;
+	}
+
+	function saveLastReadPage(pageId) {
+		if(pageId === void 0) {
+			for(let i = 0; i < pages.length; i++) {
+				if(pages[i].offset > window.scrollY + 10) {
+					pageId = i - 1;
+					break;
 				}
 			}
 		}
+		if(pageId !== lastReadPage) {
+			lastReadPage = pageId;
+			doQuery({
+				data  : {
+					action	:	'update_entry',
+					values	:	{
+						id	:	$('.reader-manga-pages-container').data('id'),
+						last_read_page	:	pageId,
+					},
+				},
+				filter	:	function(data) {
+					data.fragments = data.fragments.filter(function(fragment) {
+						return fragment.type !== 'message';
+					});
+					return data;
+				},
+			});
+		}
 	}
-});
+
+	function fitMangaPage() {
+		const windowHeight = $(window).height();
+	  pages.map(function() {
+			this.el.css({height: windowHeight});
+			this.offset = this.el.offset().top;
+		});
+	}
+
+	function ScrollToAdjacent(offset, basePage) {
+		if(basePage === void 0) {
+			basePage = lastReadPage;
+		}
+		basePage += offset;
+		if(basePage >= pages.length - 1 || basePage < 0) {
+			return;
+		}
+		isAutoScrolling = true;
+		$('html').stop().animate(
+			{scrollTop: pages[basePage].offset},
+			200,
+			function() {isAutoScrolling = false;},
+		);
+		saveLastReadPage(basePage);
+	}
+}
+
+// reader.php
+{
+
+	let isEnabled = false;
+
+	window.addEventListener('afterNavigation', function() {
+	  if(!isEnabled && currentView === 'reader') {
+			start();
+		}
+	});
+	window.addEventListener('beforeNavigation', function() {
+		if(isEnabled && currentView !== 'reader') {
+			stop();
+		}
+	});
+
+	function start() {
+		// refresh last read page
+		$(document).on('click.reader', '.reader-manga-file', function() {
+			const t = $(this);
+			let text = t.text();
+			text = text.replace(/(^\s*|\s*$)/g,'');
+			const isLastRead = t.find('.reader-manga-file-last-read').length > 0;
+			if(!isLastRead) {
+				let args = {
+					data  : {
+						action	:	'update_entry',
+						values	:	{
+							id	:	t.parents('.reader-manga-file-list').data('id'),
+							last_read_file	:	text,
+							last_read_page	:	0.
+						},
+					},
+					filter	:	function(data) {
+						data.reader_skip_refresh = true;
+						return data;
+					}
+				};
+				resetLastReadPage = true;
+				doQuery(args);
+			}
+		});
+
+		// refresah view on read progress update
+		window.addEventListener('ajaxRequestDone', refreshFileList);
+
+		isEnabled = true;
+	}
+
+	function stop() {
+		$(document).off('.reader');
+		window.removeEventListener('ajaxRequestDone', refreshFileList);
+		isEnabled = false;
+	}
+
+	function refreshFileList(event) {
+		if(
+			event.detail.reader_skip_refresh
+			|| currentView !== 'reader'
+			|| event.detail.type !== 'update_entry'
+			|| !event.detail.success
+		) {
+			return;
+		}
+		doQuery({
+			data	: {
+				action	:	'display_view',
+				values	:	{
+					name	:	'reader',
+					value	:	$('.reader-manga-file-list').data('id'),
+				},
+			}
+		});
+	}
 
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const ajaxUrl = $('body').data('ajax-url');
+let currentView = 'home';
 
 function throttle(ms, callback) {
 	var lastCall=0;
@@ -21,149 +22,139 @@ function throttle(ms, callback) {
 	};
 }
 
-// update the DOM based on the ajax response
-function handleFragments(data) {
-	if(data.fragments !== void 0) {
-		const f = data.fragments;
-		for(const fragment in f) {
-			switch(f[fragment].type) {
-			case 'after':
-				$(f[fragment].element).after(f[fragment].html);
-				break;
-			case 'append':
-				$(f[fragment].element).append(f[fragment].html);
-				break;
-			case 'delete':
-				$(f[fragment].element).remove();
-				break;
-			case 'update':
-				$(f[fragment].element).html(f[fragment].html);
-				break;
-			case 'replace':
-				$(f[fragment].element).after(f[fragment].html).remove();
-				break;
-			case 'location':
-				window.history.pushState({}, '', f[fragment].url);
-				break;
-			case 'message':
-				createMessage(f[fragment].html);
-				break;
-			case 'refresh':
-				location.reload();
-				break;
+// interact with the database through ajax
+const doQuery = (function() {
+	let currentQueryIds = {};
+
+	// update the DOM based on the ajax response
+	function handleFragments(data) {
+		if(data.fragments !== void 0) {
+			const f = data.fragments;
+			for(const fragment in f) {
+				switch(f[fragment].type) {
+				case 'after':
+					$(f[fragment].element).after(f[fragment].html);
+					break;
+				case 'append':
+					$(f[fragment].element).append(f[fragment].html);
+					break;
+				case 'delete':
+					$(f[fragment].element).remove();
+					break;
+				case 'update':
+					$(f[fragment].element).html(f[fragment].html);
+					break;
+				case 'replace':
+					$(f[fragment].element).after(f[fragment].html).remove();
+					break;
+				case 'location':
+					window.history.pushState({}, '', f[fragment].url);
+					break;
+				case 'message':
+					createMessage(f[fragment].html);
+					break;
+				case 'refresh':
+					location.reload();
+					break;
+				}
 			}
 		}
 	}
-}
 
+	return function(p) {
+		if(p.data !== void 0) {
+			const queryId = (currentQueryIds[p.data.action] === void 0)
+				? 0 : currentQueryIds[p.data.action] + 1;
+			currentQueryIds[p.data.action] = queryId;
+			let request = $.ajax({
+				method   : 'post',
+				url      : ajaxUrl,
+				data     : p.data,
+				dataType : 'json',
+			});
+			request.done(function(data) {
+				// prevent calling an action if a more recent query was issued
+				if(currentQueryIds[p.data.action] === queryId) {
+					if(p.filter !== void 0) data = p.filter(data);
+					handleFragments(data);
+					if(p.callback !== void 0) p.callback(data);
+				}
+				window.dispatchEvent(
+					new CustomEvent('ajaxRequestDone', {detail:data})
+				);
+			});
+		}
+	};
+})();
 
-// interact with the database through ajax
-let currentQueryIds = {};
-function doQuery(p) {
-	if(p.data !== void 0) {
-		const queryId = (currentQueryIds[p.data.action] === void 0)
-			? 0 : currentQueryIds[p.data.action] + 1;
-		currentQueryIds[p.data.action] = queryId;
-		let request = $.ajax({
-			method   : 'post',
-			url      : ajaxUrl,
-			data     : p.data,
-			dataType : 'json',
+{
+	// redirecting forms to use AJAX
+	$(document).on('submit', '.ajax-form', function(e) {
+	  e.preventDefault();
+	  const t = $(this);
+	  const formData = new FormData(this);
+	  const action = t.data('form-action');
+	  let values = {};
+	  t.find('input').map(function(i, e) {
+			const $e = $(e);
+			const name = $e.attr('name');
+			values[name] = $e.val();
 		});
-		request.done(function(data) {
-			// prevent calling an action if a more recent query was issued
-			if(currentQueryIds[p.data.action] === queryId) {
-				if(p.filter !== void 0) data = p.filter(data);
-				handleFragments(data);
-				if(p.callback !== void 0) p.callback(data);
-			}
-			window.dispatchEvent(
-				new CustomEvent('ajaxRequestDone', {detail:data})
-			);
-		});
-	}
-}
-
-
-// redirecting forms to use AJAX
-$(document).on('submit', '.ajax-form', function(e) {
-  e.preventDefault();
-
-  const t = $(this);
-  const formData = new FormData(this);
-  const action = t.data('form-action');
-  let values = {};
-
-  t.find('input').map(function(i, e) {
-		const $e = $(e);
-		const name = $e.attr('name');
-		values[name] = $e.val();
-	});
-
-  doQuery({
-    data  : {
-      action,
-      values,
-    }
-  });
-
-});
-
-
-// adding entries
-$(document).on('click', '.add-entry', function() {
-	doQuery({
-    data  : {
-      action  : 'add_entry',
-    }
-  });
-});
-
-
-// removing entries
-$(document).on('click', '.remove-entry', function() {
-	const id = $(this).data('id');
-	doQuery({
-    data  : {
-      action  : 'remove_entry',
-			values	:	{id},
-    }
-  });
-});
-
-
-// navigation links
-let currentView = 'home';
-$(document).on('click', '.navigation-link', function() {
-	const name = $(this).data('target');
-	const value = $(this).data('value');
-	doQuery({
-    data  : {
-      action  : 'display_view',
-			values	:	{
-				name,
-				value,
+	  doQuery({
+	    data  : {
+	      action,
+	      values,
+	    }
+	  });
+	})
+	// adding entries
+	.on('click', '.add-entry', function() {
+		doQuery({
+	    data  : {
+	      action  : 'add_entry',
+	    }
+	  });
+	})
+	// removing entries
+	.on('click', '.remove-entry', function() {
+		const id = $(this).data('id');
+		doQuery({
+	    data  : {
+	      action  : 'remove_entry',
+				values	:	{id},
+	    }
+	  });
+	})
+	// navigation links
+	.on('click', '.navigation-link', function() {
+		const name = $(this).data('target');
+		const value = $(this).data('value');
+		doQuery({
+	    data  : {
+	      action  : 'display_view',
+				values	:	{
+					name,
+					value,
+				},
+	    },
+			filter	:	(function(name) { return function(data) {
+				if(data.success) {
+					currentView = name;
+				}
+				window.dispatchEvent(
+					new CustomEvent('beforeNavigation', {detail:{name, value}})
+				);
+				return data;
+			}})(name),
+			callback	:	function() {
+				window.dispatchEvent(
+					new CustomEvent('afterNavigation', {detail:{name, value}})
+				);
 			},
-    },
-		filter	:	(function(name) { return function(data) {
-			if(data.success) {
-				currentView = name;
-			}
-			window.dispatchEvent(
-				new CustomEvent('beforeNavigation', {detail:{name, value}})
-			);
-			return data;
-		}})(name),
-		callback	:	function() {
-			window.dispatchEvent(
-				new CustomEvent('afterNavigation', {detail:{name, value}})
-			);
-		},
-  });
-});
-
-
-// show hidden sections
-$(document).on('click', '.show-more', function() {
-	$($(this).toggleClass('active').data('target')).slideToggle(300);
-});
+	  });
+	});
+	// show hidden sections
+	.on('click', '.show-more', function() {
+		$($(this).toggleClass('active').data('target')).slideToggle(300);
+	});
+}
