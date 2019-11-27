54dbafd4f5965db4fc96cb546b0a1f24ce3807cd
weterynarzfred <weterynarzfred@gmail.com>
2019-11-27 15:04:00 +0100

    host jquery

--- view/part/header.php
+++ view/part/header.php
@@ -5,7 +5,7 @@
     <title></title>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <meta name="viewport" content="width=device-width" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="./vendor/jquery.min.js"></script>
     <link rel="shortcut icon" type="image/png" href="./favicon.png"/>
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
     <link rel="stylesheet" type="text/css" href="./css/base.css" />
